678274ea5c116ea82c0184a47d4f958e8d569857
abendsen8-design <abendsen8@gmail.com>
2026-04-04 17:29:58 +0800

    存入大部分可能用到的汉字

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 #include "lv_port_indev_template.h"
 
 extern const lv_font_t font0;
+extern const lv_font_t test;
 
-void Hang2Hang();
 void testfont();
+
 
 int main()
 {
@@ -19,8 +20,10 @@ int main()
 		lv_init();
 		lv_port_disp_init();
 		lv_port_indev_init();
-		
+	
 		testfont();
+		
+
 
 		while(1){
 			
@@ -34,17 +37,11 @@ int main()
 
 /*我只是一个用来测试字体的函数*/
 void testfont()
-{
-	lv_obj_t *testlabel = lv_label_create(lv_scr_act());
-	lv_obj_set_width(testlabel, 300);
-	lv_obj_set_height(testlabel, 150);
-
-	lv_style_t style;
-	lv_style_init(&style);
-	lv_style_set_text_font(&style, &font0);
-	lv_obj_add_style(testlabel,&style,0);
-
-	lv_label_set_text(testlabel, "一二三四五六七八九十年月日周0123456789第SNABCD楼层东西取消确认查询重
-							置详情至空闲占用教室节次借用记录条起始结束用途日期参与人数联系电话容量
-							设备要求下步选择不能为空错误用户名密码登录请输入情况预览华中科技大学课设！。：-");
+{		
+		lv_obj_t *testlabel = lv_label_create(lv_scr_act());
+		lv_obj_set_width(testlabel, 300);
+		lv_obj_set_height(testlabel, 150);
+		lv_obj_set_style_text_font(testlabel, &font0, 0);
+		lv_label_set_text(testlabel, "华科C课设");
 }
+
